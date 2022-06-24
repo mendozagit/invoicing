ae9dd57519d33ebf385e0e04e35795299d16edbe
Jesús Mendoza <mendoza.git@gmail.com>
2022-06-24 18:24:26 -0500

    InvoiceService properties - added

--- src/Invoicing/Servicies/InvoiceService.cs
+++ src/Invoicing/Servicies/InvoiceService.cs
@@ -10,11 +10,12 @@ namespace Invoicing.Servicies;
 
 public class InvoiceService : IComputable
 {
-    public Invoice Invoice { get; }
+    private readonly Invoice _invoice;
+
 
     public InvoiceService()
     {
-        Invoice = new Invoice();
+        _invoice = new Invoice();
     }
 
 
@@ -58,7 +59,7 @@ public class InvoiceService : IComputable
         };
 
 
-        Invoice.InvoiceItems.Add(invoiceItem);
+        _invoice.InvoiceItems.Add(invoiceItem);
     }
 
     /// <summary>
@@ -68,7 +69,7 @@ public class InvoiceService : IComputable
     public void AddInvoiceItem(InvoiceItem invoiceItem)
     {
         //_invoice.InvoiceItems ??= new List<InvoiceItem>();
-        Invoice.InvoiceItems.Add(invoiceItem);
+        _invoice.InvoiceItems.Add(invoiceItem);
     }
 
     /// <summary>
@@ -82,7 +83,7 @@ public class InvoiceService : IComputable
             throw new ArgumentNullException(nameof(invoiceItems),
                 "The list of items on the invoice must contain at least one item");
 
-        Invoice.InvoiceItems.AddRange(invoiceItems);
+        _invoice.InvoiceItems.AddRange(invoiceItems);
     }
 
 
@@ -105,7 +106,7 @@ public class InvoiceService : IComputable
             Encoding = Encoding.UTF8,
             Indent = true
         };
-        Serializer<Invoice>.SerializeToFile(Invoice, filePath, SerializerHelper.Namespaces, settings);
+        Serializer<Invoice>.SerializeToFile(_invoice, filePath, SerializerHelper.Namespaces, settings);
     }
 
 
@@ -124,7 +125,7 @@ public class InvoiceService : IComputable
             Encoding = Encoding.UTF8,
             Indent = false
         };
-        var xml = Serializer<Invoice>.Serialize(Invoice, SerializerHelper.Namespaces, settings);
+        var xml = Serializer<Invoice>.Serialize(_invoice, SerializerHelper.Namespaces, settings);
 
         return xml.Clean();
     }
@@ -141,6 +142,226 @@ public class InvoiceService : IComputable
     /// </summary>
     public void Compute()
     {
-        Invoice.Compute();
+        _invoice.Compute();
     }
+
+    #region Properties
+
+    private InvoiceVersion _invoiceVersion;
+    private string? _invoiceSerie;
+    private string? _invoiceNuber;
+    private string? _invoiceDate;
+    private string? _signatureValue;
+    private string? _paymentForm;
+    private string? _certificateNumber;
+    private string? _certificateB64;
+    private string? _paymentConditions;
+    private decimal _subtotal;
+    private decimal _discount;
+    private string? _currency;
+    private decimal _exchangeRate;
+    private decimal _total;
+    private InvoiceType _invoiceTypeId;
+    private string? _exportId;
+    private string? _paymentMethodId;
+    private string? _expeditionZipCode;
+    private string? _pacConfirmation;
+
+
+    /// <summary>
+    /// Atributo requerido con valor prefijado a 4.0 que indica la versión del estándar bajo el que se encuentra expresado el comprobante.
+    /// </summary>
+
+    public InvoiceVersion InvoiceVersion
+    {
+        get => _invoiceVersion;
+        set => _invoiceVersion = _invoice.InvoiceVersion = value;
+    }
+
+
+    /// <summary>
+    /// Atributo opcional para precisar la serie para control interno del contribuyente. Este atributo acepta una cadena de caracteres.
+    /// </summary>
+
+    public string? InvoiceSerie
+    {
+        get => _invoiceSerie;
+        set => _invoiceSerie = _invoice.InvoiceSerie = value;
+    }
+
+    /// <summary>
+    /// Atributo opcional para control interno del contribuyente que expresa el folio del comprobante, acepta una cadena de caracteres.
+    /// </summary>
+
+    public string? InvoiceNuber
+    {
+        get => _invoiceNuber;
+        set => _invoiceNuber = _invoice.InvoiceNuber = value;
+    }
+
+
+    /// <summary>
+    /// Atributo requerido para la expresión de la fecha y hora de expedición del Comprobante Fiscal Digital por Internet. Se expresa en la forma AAAA-MM-DDThh:mm:ss y debe corresponder con la hora local donde se expide el comprobante.
+    /// </summary>
+
+    public string? InvoiceDate
+    {
+        get => _invoiceDate;
+        set => _invoiceDate = _invoice.InvoiceDate = value;
+    }
+
+    /// <summary>
+    /// Atributo requerido para contener el sello digital del comprobante fiscal, al que hacen referencia las reglas de resolución miscelánea vigente. El sello debe ser expresado como una cadena de texto en formato Base 64.
+    /// </summary>
+
+    public string? SignatureValue
+    {
+        get => _signatureValue;
+        set => _signatureValue = _invoice.SignatureValue = value;
+    }
+
+
+    /// <summary>
+    /// Atributo condicional para expresar la clave de la forma de pago de los bienes o servicios amparados por el comprobante.
+    /// </summary>
+
+    public string? PaymentForm
+    {
+        get => _paymentForm;
+        set => _paymentForm = _invoice.PaymentForm = value;
+    }
+
+    /// <summary>
+    /// Atributo requerido para expresar el número de serie del certificado de sello digital que ampara al comprobante, de acuerdo con el acuse correspondiente a 20 posiciones otorgado por el sistema del SAT.
+    /// </summary>
+
+    public string? CertificateNumber
+    {
+        get => _certificateNumber;
+        set => _certificateNumber = _invoice.CertificateNumber = value;
+    }
+
+    /// <summary>
+    /// Atributo requerido que sirve para incorporar el certificado de sello digital que ampara al comprobante, como texto en formato base 64.
+    /// </summary>
+    public string? CertificateB64
+    {
+        get => _certificateB64;
+        set => _certificateB64 = _invoice.CertificateB64 = value;
+    }
+
+
+    /// <summary>
+    /// Atributo condicional para expresar las condiciones comerciales aplicables para el pago del comprobante fiscal digital por Internet. Este atributo puede ser condicionado mediante atributos o complementos.
+    /// </summary>
+    public string? PaymentConditions
+    {
+        get => _paymentConditions;
+        set => _paymentConditions = _invoice.PaymentConditions = value;
+    }
+
+
+    /// <summary>
+    /// Atributo requerido para representar la suma de los importes de los conceptos antes de descuentos e impuesto. No se permiten valores negativos.
+    /// </summary>
+    public decimal Subtotal
+    {
+        get => _subtotal;
+        set => _subtotal = _invoice.Subtotal = value;
+    }
+
+    /// <summary>
+    /// Atributo condicional para representar el importe total de los descuentos aplicables antes de impuestos. No se permiten valores negativos. Se debe registrar cuando existan conceptos con descuento.
+    /// </summary>
+    public decimal Discount
+    {
+        get => _discount;
+        set => _discount = _invoice.Discount = value;
+    }
+
+    /// <summary>
+    /// Atributo requerido para identificar la clave de la moneda utilizada para expresar los montos, cuando se usa moneda nacional se registra MXN. Conforme con la especificación ISO 4217.
+    /// </summary>
+
+    public string? Currency
+    {
+        get => _currency;
+        set => _currency = _invoice.Currency = value;
+    }
+
+    /// <summary>
+    /// Atributo condicional para representar el tipo de cambio FIX conforme con la moneda usada. Es requerido cuando la clave de moneda es distinta de MXN y de XXX. El valor debe reflejar el número de pesos mexicanos que equivalen a una unidad de la divisa señalada en el atributo moneda. Si el valor está fuera del porcentaje aplicable a la moneda tomado del catálogo c_Moneda, el emisor debe obtener del PAC que vaya a timbrar el CFDI, de manera no automática, una clave de confirmación para ratificar que el valor es correcto e integrar dicha clave en el atributo Confirmacion.
+    /// </summary>
+
+    public decimal ExchangeRate
+    {
+        get => _exchangeRate;
+        set => _exchangeRate = _invoice.ExchangeRate = value;
+    }
+
+    /// <summary>
+    /// Atributo requerido para representar la suma del subtotal, menos los descuentos aplicables, más las contribuciones recibidas (impuestos trasladados - federales y/o locales, derechos, productos, aprovechamientos, aportaciones de seguridad social, contribuciones de mejoras) menos los impuestos retenidos federales y/o locales. Si el valor es superior al límite que establezca el SAT en la Resolución Miscelánea Fiscal vigente, el emisor debe obtener del PAC que vaya a timbrar el CFDI, de manera no automática, una clave de confirmación para ratificar que el valor es correcto e integrar dicha clave en el atributo Confirmacion. No se permiten valores negativos.
+    /// </summary>
+
+    public decimal Total
+    {
+        get => _total;
+        set => _total = _invoice.Total = value;
+    }
+
+    /// <summary>
+    /// Atributo requerido para expresar la clave del efecto del comprobante fiscal para el contribuyente emisor.
+    /// </summary>
+
+    public InvoiceType InvoiceTypeId
+    {
+        get => _invoiceTypeId;
+        set => _invoiceTypeId = _invoice.InvoiceTypeId = value;
+    }
+
+
+    /// <summary>
+    /// Atributo requerido para expresar si el comprobante ampara una operación de exportación.
+    /// </summary>
+
+    public string? ExportId
+    {
+        get => _exportId;
+        set => _exportId = _invoice.ExportId = value;
+    }
+
+
+    /// <summary>
+    /// Atributo condicional para precisar la clave del método de pago que aplica para este comprobante fiscal digital por Internet, conforme al Artículo 29-A fracción VII incisos a y b del CFF.
+    /// </summary>
+
+    public string? PaymentMethodId
+    {
+        get => _paymentMethodId;
+        set => _paymentMethodId = _invoice.PaymentMethodId = value;
+    }
+
+
+    /// <summary>
+    /// Atributo requerido para incorporar el código postal del lugar de expedición del comprobante (domicilio de la matriz o de la sucursal).
+    /// </summary>
+
+    public string? ExpeditionZipCode
+    {
+        get => _expeditionZipCode;
+        set => _expeditionZipCode = _invoice.ExpeditionZipCode = value;
+    }
+
+
+    /// <summary>
+    /// Atributo condicional para registrar la clave de confirmación que entregue el PAC para expedir el comprobante con importes grandes, con un tipo de cambio fuera del rango establecido o con ambos casos. Es requerido cuando se registra un tipo de cambio o un total fuera del rango establecido.
+    /// </summary>
+
+    public string? PacConfirmation
+    {
+        get => _pacConfirmation;
+        set => _pacConfirmation = _invoice.PacConfirmation = value;
+    }
+
+    #endregion
 }
